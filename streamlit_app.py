acc2bb3a94b7dd17fd9b0b6887e3fdd5bc754225
Nabin Ghorui <153352019+dev-nabin88@users.noreply.github.com>
2024-11-02 19:59:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ def remove_background_and_add_color(upload, bg_color):
         'Purple': (128, 0, 128),
         'Red': (255, 0, 0),
         'Yellow': (255, 255, 0)
+        'Blue': (76, 137, 248)
     }
     
     # Open the uploaded image
